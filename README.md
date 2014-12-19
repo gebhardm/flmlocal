e2cf1c3b0a67ba7e11250f0e172ed747ece07695
Markus Gebhard <markus.gebhard@web.de>
2014-12-19 17:54:30 +0100

    beautify code and update license

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 #FLM local visualizations
-This is a native implementation of the [Justgage](http:/justgage.com) gauges to be used in the [Fliuksometer's](http://flukso.net) AngularJS user interface.
-To utilize this implementation, copy the content of the [www/](www/) folder to your FLM with firmware version >2.4.<br>
-Use the linux/OSX command **scp** for this purpose; for windows use WinSCP.
+This is a native implementation of the [Justgage](http:/justgage.com) gauges and a plain panel to be used in the [Fluksometer's](http://flukso.net) AngularJS based user interface. It sits on top of the [Paho JavaScript client](https://eclipse.org/paho/clients/js/).<br/>
+To utilize this implementation, copy the content of the [www/](www/) folder to your Fluksometer with firmware version >2.4.<br>
+Use the linux/OSX command **scp** for this purpose; for windows use [WinSCP](http://winscp.net).
 
 `scp -r * root@<FLM ip address>:/www/`
 
-You are prompted for the root's password, then all necessary files are transferred (option -r)
+You are prompted for the root's password, then all necessary files are transferred (recursively through option -r)
 
-By that you gain direct access to a local gauge visualization directly from the FLM's landing page navigation when calling
+By that you gain direct access to a local gauge and panel visualization directly from the Fluksometer's landing page navigation when calling
 
 `<flm ip address>`
 
--- www/scripts/controllers/gauge.js
+++ www/scripts/controllers/gauge.js
@@ -1,161 +1,181 @@
 /*
- * Copyright (c) 2014 Markus Gebhard <markus.gebhard@web.de>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ The MIT License (MIT)
+ 
+ Copyright (c) 2014 Markus Gebhard <markus.gebhard@web.de>
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
  */
-
-'use strict';
+"use strict";
 
 // link to the web server's IP address for MQTT socket connection
 var client;
-var reconnectTimeout = 2000;
-var port = 8083; // the FLM's web socket port from mosquitto
-var wsID = "FLM" + parseInt(Math.random() * 100, 10); // get "different" websocketIDs
 
+var reconnectTimeout = 2e3;
+
+var port = 8083;
+
+// the FLM's web socket port from mosquitto
+var wsID = "FLM" + parseInt(Math.random() * 100, 10);
+
+// get "different" websocketIDs
 var gauge = {}, display = {}, numGauges = 0;
+
 var row = [];
 
 // the part of the AngularJS application that handles the gauges
 var app = angular.module("flmUiApp");
 
 app.controller("GaugeCtrl", function($scope) {
-	$scope.debug = false;
-	$scope.alerts = [];
-	$scope.gauges = [];
-	$scope.message = "";
-    
-	$scope.closeAlert = function(index) {
-		$scope.alerts.splice(index, 1);
-	};
+    $scope.debug = false;
+    $scope.alerts = [];
+    $scope.gauges = [];
+    $scope.message = "";
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+    function pushError(error) {
+        $scope.alerts.push({
+            type: "error",
+            msg: error
+        });
+    }
+    // the web socket connect function
+    function mqttConnect() {
+        client = new Paho.MQTT.Client(location.hostname, port, "", wsID);
+        var options = {
+            timeout: 3,
+            onSuccess: onConnect,
+            onFailure: function(message) {
+                setTimeout(mqttConnect, reconnectTimeout);
+            }
+        };
+        // define callback routines
+        client.onConnectionLost = onConnectionLost;
+        client.onMessageArrived = onMessageArrived;
+        client.connect(options);
+    }
+    // event handler on connection established
+    function onConnect() {
+        client.subscribe("/sensor/#");
+    }
+    // event handler on connection lost
+    function onConnectionLost(responseObj) {
+        setTimeout(mqttConnect, reconnectTimeout);
+        if (responseObj.errorCode !== 0) console.log("onConnectionLost:" + responseObj.errorMessage);
+    }
+    function onMessageArrived(mqttMsg) {
+        // split the received message at the slashes
+        var topic = mqttMsg.destinationName.split("/");
+        var payload = mqttMsg.payloadString;
+        // pass sensor message to the html part
+        $scope.$apply(function() {
+            $scope.message = mqttMsg.destinationName + ", " + payload;
+        });
+        // the sensor message type is the third value of the topic
+        var msgType = topic[3];
+        // gauge or counter
+        var sensor = topic[2];
+        // the sensor ID
+        var value = JSON.parse(payload);
+        // the transferred payload
+        var unit = "";
+        // now comput the received mqttMessage
+        switch (msgType) {
+          case "config":
+            break;
 
-	function pushError(error) {
-		$scope.alerts.push({
-			type: "error",
-			msg: error
-		});
-	};
+          case "gauge":
+            // handle the payload to obtain gauge values
+            if (value.length == null) {
+                gauge[sensor] = value;
+                unit = "";
+            } else {
+                switch (value.length) {
+                  case 1:
+                    gauge[sensor] = value[0];
+                    unit = "";
+                    break;
 
-	// the web socket connect function
-	function mqttConnect() {
-		client = new Paho.MQTT.Client(location.hostname, port, "", wsID);
-		var options = {
-			timeout : 3,
-			onSuccess : onConnect,
-			onFailure : function(message) { setTimeout(mqttConnect, reconnectTimeout); }
-		};
-		// define callback routines
-		client.onConnectionLost = onConnectionLost;
-		client.onMessageArrived = onMessageArrived;
-		client.connect(options);
-	};
+                  case 2:
+                    gauge[sensor] = value[0];
+                    unit = value[1];
+                    break;
 
-	// event handler on connection established
-	function onConnect() {
-		client.subscribe("/sensor/#");
-	};
+                  case 3:
+                    var date = new Date(value[0] * 1e3);
+                    // the timestamp
+                    var now = new Date().getTime();
+                    if (now / 1e3 - value[0] > 60) value[1] = 0;
+                    // if too old, set to 0
+                    gauge[sensor] = value[1];
+                    unit = value[2];
+                    break;
 
-	// event handler on connection lost
-	function onConnectionLost(responseObj) {
-		setTimeout(mqttConnect, reconnectTimeout);
-		if (responseObj.errorCode !== 0)
-			console.log("onConnectionLost:" + responseObj.errorMessage);
-	};
+                  default:
+                    break;
+                }
+            }
+            ;
+            // now build the gauge display
+            if (display[sensor] == null) {
+                numGauges++;
+                var rowIndex = Math.floor((numGauges - 1) / 2);
+                var colIndex = numGauges % 2;
+                // use a trick to scale the first gauge at 50%
+                if (row[rowIndex] == null) row[rowIndex] = new Array();
+                if (numGauges > 1 && colIndex == 0) row[rowIndex].pop();
+                row[rowIndex].push({
+                    id: sensor
+                });
+                if (colIndex == 1) row[rowIndex].push({});
+                $scope.$apply(function() {
+                    $scope.gauges = row;
+                });
+                var limit = 0, decimals = 0;
+                if (unit == "W") limit = 250; else if (unit == "°C") {
+                    limit = 50;
+                    decimals = 2;
+                } else limit = 100;
+                limit = gauge[sensor] > limit ? gauge[sensor] : limit;
+                display[sensor] = new JustGage({
+                    id: sensor,
+                    value: gauge[sensor],
+                    title: sensor,
+                    label: unit,
+                    min: 0,
+                    max: limit,
+                    decimals: decimals
+                });
+            }
+            // now show the current gauge value - set gaugeMax newly if required
+            if (gauge[sensor] > display[sensor].txtMaximum) {
+                display[sensor].refresh(gauge[sensor], gauge[sensor]);
+            }
+            display[sensor].refresh(gauge[sensor]);
+            break;
 
-	function onMessageArrived(mqttMsg) {
-		// split the received message at the slashes
-		var topic = mqttMsg.destinationName.split('/');
-		var payload = mqttMsg.payloadString;
-		// pass sensor message to the html part
-		$scope.$apply(function () {
-			$scope.message = mqttMsg.destinationName + ", " + payload;
-		});
-		// the sensor message type is the third value of the topic
-		var msgType = topic[3]; // gauge or counter
-		var sensor = topic[2]; // the sensor ID
-		var value = JSON.parse(payload); // the transferred payload
-		var unit = '';
-		// now comput the received mqttMessage
-		switch (msgType) {
-			case 'config' : break;
-			case 'gauge' :
-				// handle the payload to obtain gauge values
-				if (value.length == null) {
-					gauge[sensor] = value;
-					unit = '';
-				} else {
-					switch (value.length) {
-						case 1:
-							gauge[sensor] = value[0];
-							unit = '';
-							break;
-						case 2:
-							gauge[sensor] = value[0];
-							unit = value[1];
-							break;
-						case 3:
-							var date = new Date(value[0] * 1000); // the timestamp
-							var now = new Date().getTime();
-							if ((now / 1000 - value[0]) > 60)
-								value[1] = 0; // if too old, set to 0
-							gauge[sensor] = value[1];
-							unit = value[2];
-							break;
-						default: break;
-					};
-				};
-				// now build the gauge display
-				if (display[sensor] == null) {
-					numGauges++;
-					var rowIndex = Math.floor((numGauges-1)/2);
-					var colIndex = numGauges % 2;
-					// use a trick to scale the first gauge at 50%
-					if (row[rowIndex] == null) row[rowIndex] = new Array();
-					if ((numGauges > 1) && (colIndex == 0)) row[rowIndex].pop();
-					row[rowIndex].push({id:sensor});
-					if (colIndex == 1) row[rowIndex].push({});
-					$scope.$apply(function() {
-						$scope.gauges = row;
-					});
-					var limit = 0, decimals = 0;
-					if (unit == 'W')
-						limit = 250;
-					else if (unit == '°C') {
-						limit = 50;
-						decimals = 2;
-					} else
-						limit = 100;
-					limit = (gauge[sensor]>limit?gauge[sensor]:limit);
-					display[sensor] = new JustGage({
-						id : sensor,
-						value : gauge[sensor],
-						title : sensor,
-						label : unit,
-						min : 0,
-						max : limit,
-						decimals : decimals
-					});
-				}
-				// now show the current gauge value - set gaugeMax newly if required
-				if (gauge[sensor] > display[sensor].txtMaximum) {
-					display[sensor].refresh(gauge[sensor], gauge[sensor]);
-				}
-				display[sensor].refresh(gauge[sensor]);
-				break;
-			case 'counter' : break;
-			default : break;
-		};
-	};
+          case "counter":
+            break;
 
-	mqttConnect();
+          default:
+            break;
+        }
+    }
+    mqttConnect();
 });
